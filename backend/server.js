5a690a37e3d894d8de542669e6c0d2d6ae909e83
Virajsinh13 <virajsinhvaghela511@gmail.com>
2026-04-07 00:55:25 +0530

    Resolved merge conflict in server.js

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,10 @@ connectDB();
 // 🔥 Use routes
 app.use('/api', require('./routes/departmentRoutes'));
 app.use('/api', require('./routes/programRoutes'));
-<<<<<<< HEAD
+
 app.use('/api', require('./routes/courseRoutes'));
-=======
+
 // app.use('/api', require('./routes/facultyRoutes'));
->>>>>>> 22f45f3d743a4bbe030e607fc1b685ab1899a86a
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
